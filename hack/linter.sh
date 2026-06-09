2aad65c581d4ab121c8fbdc7282e6b29d5103775
mentlak <mentlak@outlook.com>
2026-06-09 14:39:51 +0100

    chore: upgrade golangci-lint to 2.12.2

--- hack/linter.sh
+++ hack/linter.sh
@@ -9,8 +9,8 @@ fi
 
 linterVersion="$(golangci-lint --version | awk '{print $4}')"
 
-if [[ ! "${linterVersion}" =~ ^1\.5[89] ]]; then
-	echo "Install GolangCI-Lint version 1.58 or 1.59"
+if [[ ! "${linterVersion}" =~ ^2\.12\. ]]; then
+	echo "Install GolangCI-Lint version 2.12.x"
   exit 1
 fi
 
